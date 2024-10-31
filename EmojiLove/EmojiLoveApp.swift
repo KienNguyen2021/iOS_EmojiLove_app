91ba454b1b546a1f2e6060a742b7e5d727372d9f
nguyen8k <56096127+nguyen8k@users.noreply.github.com>
2024-10-30 18:13:43 -0600

    Initial Commit

---
+++ EmojiLove/EmojiLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoveApp.swift
+//  EmojiLove
+//
+//  Created by Kien Nguyen on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
